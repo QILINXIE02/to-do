86d02dc9ab3af087dbf55bde295ac341bb5f7a3f
Qilin Xie <qxie@gicw.org>
2023-12-19 10:00:06 -0800

    adds colors background options and dates to react

--- README.md
+++ README.md
@@ -4,15 +4,13 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Available Scripts
 
-In the project directory, you can run:
+In the project directory, run:
 
 ### `npm start`
 
 Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
 
 ### `npm test`
 
--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const STORAGE_KEY = 'tasks';
+
 function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    // Load tasks from local storage on component mount
+    const storedTasks = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+    setTasks(storedTasks);
+  }, []);
+
+  useEffect(() => {
+    // Save tasks to local storage whenever tasks change
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = () => {
+    if (newTask.trim() !== '') {
+      const currentDate = new Date().toLocaleDateString();
+      setTasks([...tasks, { id: Date.now(), text: newTask, completed: false, date: currentDate }]);
+      setNewTask('');
+    }
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  const toggleTask = (taskId) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === taskId ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const changePageColor = () => {
+    const colorInput = document.getElementById('colorInput').value;
+    document.body.style.backgroundColor = colorInput;
+  };
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.body.classList.toggle('dark-mode');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${darkMode ? 'dark-mode' : ''}`}>
+      <h1>To-Do List</h1>
+      <div className="button-container">
+        <label htmlFor="colorInput">Set page background color:</label>
+        <input type="text" id="colorInput" placeholder="Enter a color" />
+        <button onClick={changePageColor}>Change Color</button>
+        <button id="toggle-mode" className="highlight" onClick={toggleDarkMode}>
+          {darkMode ? 'Light Mode' : 'Dark Mode'}
+        </button>
+        <br />
+        <br />
+      </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Add a new task..."
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add</button>
+      </div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id} className={task.completed ? 'completed' : ''}>
+            <span>{task.text} - {task.date}</span>
+            <div>
+              <button onClick={() => toggleTask(task.id)}>
+                {task.completed ? 'Undo' : 'Complete'}
+              </button>
+              <button onClick={() => deleteTask(task.id)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
